3d6d2a6ff1d46b8cf2e61aea12c2be6432d5d055
Nazibul Haque <elegant.nazib@gmail.com>
2018-03-11 04:48:33 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,10 @@
+function forloop(array){
+  for (var i = 0; i<array.length; i++) {
+    if(i==1){
+      array.push("I am 1 strange loop");
+    }
+    else {
+      array.push("I am ${i} strange loops}");
+    }
+  }
+}
