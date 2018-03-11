a1d6efe4dd813d88a6b2421565a61c059da18d0f
Nazibul Haque <elegant.nazib@gmail.com>
2018-03-11 04:51:40 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forloop(array){
-  for (var i = 0; i<array.length; i++) {
-    if(i==1){
-      array.push("I am 1 strange loop");
+  for (var i = 0; i<25; i++) {
+    if(i===1){
+      array.push(`I am 1 strange loop`);
     }
     else {
-      array.push("I am ${i} strange loops}");
+      array.push(`I am ${i} strange loops`);
     }
   }
 }
